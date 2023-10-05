8f2c98026b95fa57f45d8c2bdb5e1cbb1a9b049d
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-10-05 20:40:01 +0600

    add deployment to docker

---
+++ nonsudo_deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function send_notification() {
+    echo "$1"
+}
+
+function assert_success() {
+    "${@}"
+    local status=${?}
+    if [ ${status} -ne 0 ]; then
+        send_notification "### Error ${status} at: ${BASH_LINENO[*]} ###"
+        exit ${status}
+    fi
+}
+
+app=booru_grabber_bot
+version="`grep ./gradle.properties -e "^version=" | sed -e "s/version=\(.*\)/\1/"`"
+server=insanusmokrassar
+
+assert_success docker build -t $app:"$version" .
+assert_success docker tag $app:"$version" $server/$app:$version
+assert_success docker tag $app:"$version" $server/$app:latest
+assert_success docker push $server/$app:$version
+assert_success docker push $server/$app:latest
